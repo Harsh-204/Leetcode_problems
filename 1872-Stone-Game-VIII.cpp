99ef0cd910b2799b438e304f4f556d153e3a0454
Harsh Sahu <harsh.24bai10561@vitbhopal.ac.in>
2026-08-24 16:38:08 +0530

    [1872] [Time Beats: 64.28%] [Memory Beats: 49.63%] - LeetPush

---
+++ 1872-Stone-Game-VIII.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        vector<long long> pref(n, 0);
+        pref[0] = stones[0];
+        for (int i = 1; i < n; ++i) {
+            pref[i] = pref[i - 1] + stones[i];
+        }
+
+        long long ans = pref[n - 1];
+        for (int i = n - 2; i >= 1; --i) {
+            ans = max(ans, pref[i] - ans);
+        }
+        return ans;
+    }
+};
